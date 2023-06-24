893b8e2c7dffb0320fb9b5e8b3c3b72298517870
Kez Anwar <kezanwar@gmail.com>
2023-06-24 09:58:27 +0100

    setup testimonials in db.json

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 // redux
 import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import { RootState } from '@app/store/store';
+
+import Johnny from '@app/assets/johnny-testimonial.png';
+import Mary from '@app/assets/mary-testimonial.png';
+import Paul from '@app/assets/paul-testimonial.png';
 
 const Header: React.FC = (props) => {
   const { mobileNavOpen } = useSelector((state: RootState) => state.nav);
   console.log(mobileNavOpen);
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      {[Johnny, Mary, Paul].map((image) => (
+        <img src={image} alt=""></img>
+      ))}
+    </div>
+  );
 };
 
 export default Header;
--- src/types/testimonials.d.ts
+++ src/types/testimonials.d.ts
@@ -1,6 +1,7 @@
 export interface Testimonial {
+  id: string;
   author: string;
-  user: string;
+  position: string;
   content: string;
   imgUrl: string;
 }
